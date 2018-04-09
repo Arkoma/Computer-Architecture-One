534dcaa9901d47cacf2758bd932eb4558e14bbeb
Richard Reis <richardreeze1@gmail.com>
2018-04-09 15:26:57 -0700

    finished parts of tick function

--- project/ls8/cpu.js
+++ project/ls8/cpu.js
@@ -68,6 +68,7 @@ class CPU {
         // from the memory address pointed to by the PC. (I.e. the PC holds the
         // index into memory of the instruction that's about to be executed
         // right now.)
+        let IR = this.ram.read(this.reg.PC);
 
         // !!! IMPLEMENT ME
 
@@ -76,13 +77,47 @@ class CPU {
 
         // Get the two bytes in memory _after_ the PC in case the instruction
         // needs them.
+        let operandA = this.ram.read(this.reg.PC + 1);
+        let operandB = this.ram.read(this.reg.PC + 2);
 
         // !!! IMPLEMENT ME
 
         // Execute the instruction. Perform the actions for the instruction as
-        // outlined in the LS-8 spec.
+        // outlined in the LS-8 spec. 
 
         // !!! IMPLEMENT ME
+
+        //const ADD = 10101000;
+        //const AND = 10110011;
+        //const CALL = 01001000;
+        //const CMP =
+        //const DEC =
+        //const DIV =
+        const HLT = 00000001;
+        //const INC =
+        // const INT =
+        //const IRET =
+        //const JEQ = 
+        // const JGT =
+        //const JLT =
+        //const JMP =
+        //const JNE =
+        //const LD =
+        const LDI = 10011001;
+        //const MOD =
+        const MUL = 10101010;
+        //const NOP =
+        //const NOT =
+        //const OR =
+        //const POP =
+        //const PRA =
+        const PRN = 01000011;
+        //const PUSH = 
+        //const RET = 00001001;
+        //const ST = 10011010;
+        //const SUB = 10101001;
+        //const XOR = 10110010;
+
 
         // Increment the PC register to go to the next instruction. Instructions
         // can be 1, 2, or 3 bytes long. Hint: the high 2 bits of the
