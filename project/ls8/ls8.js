f645de14c172cedbbaf2cba60f1462cce3611be2
Richard Reis <richardreeze1@gmail.com>
2018-04-11 13:11:37 -0700

    finish step 7

--- project/ls8/ls8.js
+++ project/ls8/ls8.js
@@ -3,45 +3,11 @@ const CPU = require('./cpu');
 const readline = require('readline');
 const fs = require('fs');
 
-
-/**
+/**i
  * Load an LS8 program into memory
- *
- * TODO: load this from a file on disk instead of having it hardcoded
  */
 function loadMemory() {
-
-    // Hardcoded program to print the number 8 on the console
-
-  /* const program = [ // print8.ls8
-      "10011001", // LDI R0,8  Store 8 into R0
-        "00000000",
-        "00001000",
-        "01000011", // PRN R0    Print the value in R0
-        "00000000",
-        "00000001"  // HLT       Halt and quit
-    ]; */
-
-  /*  const program = [
-    //# mult.ls8
-
-    "10011001", // # LDI R0,8
-    "00000000",
-    "00001000",
-    "10011001", // # LDI R1,9
-    "00000001",
-    "00001001",
-    "10101010", // # MUL R0,R1 <---
-    "00000000",
-    "00000001",
-    "01000011", // # PRN R0
-    "00000000",
-    "00000001" // # HLT
-  
-  ];
-  */
-  // const mul = fs.readFileSync('./mult.ls8', { encoding: 'utf8' } );
-  const instream = fs.createReadStream('./mult.ls8');
+  const instream = fs.createReadStream(process.argv[2]);
   instream.on('end', () => { 
     cpu.startClock();
   });
@@ -58,10 +24,6 @@ function loadMemory() {
       cpu.poke(n, parseInt(line.slice(0,8), 2));
     }
   });
-  // Load the program into the CPU's memory a byte at a time
-  /*for (let i = 0; i < program.length; i++) {
-        cpu.poke(i, parseInt(program[i], 2));
-    } */ 
 }
 
 /**
